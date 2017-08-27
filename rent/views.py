96a616a9f887e3df12c1425d01a1bf7459a92842
whirish <lpoflynn@protonmail.ch>
2017-08-26 22:49:43 -0700

    Backend now ready to retrieve auth token & user data

--- rent/views.py
+++ rent/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, auth
 
 from .forms import LoginForm
+
+default_app = firebase_admin.initialize_app(credentials.Certificate('firebase.json'))
 
 # Create your views here.
 def index(req):
     return HttpResponse("Welcome to /rent. This will be our index app.")
 
 def auth(req):
-    default_app = firebase_admin.initialize_app(credentials.Certificate('firebase.json'))
-    form = LoginForm()
+    #form = LoginForm()
+    if req.method == "POST":
+        form = LoginForm(req.POST)
+        if form.is_valid():
+            print("Form is Valid")
+            auth = form.cleaned_data['auth_token']
+            user = auth.get_user(uid)
+
     return render(req, 'rent/auth.html', {'form': form}) 
