1d3d31e6c2742c5117ac7a26b904f58b46fbb52e
whirish <lpoflynn@protonmail.ch>
2017-08-26 19:27:31 -0700

    Firebase Admin SDK Import and Signin template rendering

--- lend/settings.py
+++ lend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'lend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['rent/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- rent/views.py
+++ rent/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import firebase_admin
+from firebase_admin import credentials
+
+from .forms import LoginForm
+
 # Create your views here.
 def index(req):
     return HttpResponse("Welcome to /rent. This will be our index app.")
+
+def auth(req):
+    default_app = firebase_admin.initialize_app(credentials.Certificate('firebase.json'))
+    form = LoginForm()
+    return render(req, 'rent/auth.html', {'form': form}) 
