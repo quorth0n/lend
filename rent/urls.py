c2a087cb0f1c293dd614c8ba0083b4bd9a5dac7c
whirish <lpoflynn@protonmail.ch>
2017-08-31 21:48:17 -0700

    User data now stored on backend in request session, generic view for product listing created

--- rent/urls.py
+++ rent/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^auth/$', views.auth, name='auth'),
+    url(r'^product/(?P<pk>[0-9]+)/$', views.ProductView.as_view(), name='plist'),
 ]
 
 
--- rent/views.py
+++ rent/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 import firebase_admin
 from firebase_admin import auth, credentials
 
 from .forms import LoginForm
+from .models import Product
 
 default_app = firebase_admin.initialize_app(credentials.Certificate('firebase.json'))
 
 # Create your views here.
 def index(req):
+    print(req.session['email'])
     return HttpResponse("Welcome to /rent. This will be our index app.")
 
 def auth(req):
     form = LoginForm()
     if req.method == "POST":
         form = LoginForm(req.POST)
-        token = firebase_admin.auth.verify_id_token(req.POST.get('auth_token', None))
-        user = firebase_admin.auth.get_user(token['uid'])
-        print(user.uid)
+
         if form.is_valid():
             print("Form is Valid")
+            token = firebase_admin.auth.verify_id_token(req.POST.get('auth_token', None))
+            user = firebase_admin.auth.get_user(token['uid'])
+            req.session['uid'] = user.uid
+            req.session['email'] = user.email
+            req.session['name'] = token.get('name')
+        #print(vars(user)) 
 
     return render(req, 'rent/auth.html', {'form': form})
+
+class ProductView(generic.DetailView):
+    model = Product
+    template_name = "rent/listing.html"
+
+
+#def plisting(req, product_id):
+    #return HttpResponse("/product/")
